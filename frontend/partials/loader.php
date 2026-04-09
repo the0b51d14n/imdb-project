12621b9f6e03d74fa4043621d73a015d55b6eef8
the0b51d14n <theo.andrimananarisoa@gmail.com>
2026-04-09 19:49:48 +0200

    ajout d'un loader

---
+++ frontend/partials/loader.php
@@ -0,0 +1,6 @@
+<div class="page-loader" id="page-loader">
+  <div class="loader-logo">SUPINFO<span style="color:#e8e8f0">.TV</span></div>
+  <div class="loader-bar">
+    <div class="loader-bar-inner"></div>
+  </div>
+</div>
